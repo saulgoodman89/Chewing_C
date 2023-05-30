85c35e223faf33edb22a8d8d86fb7dbd83abd687
EungGyo <eunggyo@hutek.net>
2023-05-30 20:43:56 +0900

    구조체 - 2

--- Pointer/struct.c
+++ Pointer/struct.c
@@ -59,6 +59,7 @@ int main() {
 
 }*/
 
+/*
 struct TEST {
 	int c;
 };
@@ -72,6 +73,127 @@ int main() {
 	pt->c = 1;
 	printf("t.c : %d \n",t.c);
 	return 0;
+ㅑ
+*/
 
+/*
+struct TEST {
+	int c;
+	int *pointer;
+};
+int main()  {
+	struct TEST t;
+	struct TEST *pt = &t;
+	int i = 0;
 
+	t.pointer = &i;
+	*t.pointer = 3;
+
+	printf("i : %d \n", i);
+
+	*pt->pointer = 4;
+
+	printf("i : %d \n",i);
+	return 0;
+}*/
+
+/*
+int add_one(int *a);
+struct TEST {
+	int c;
+};
+
+int main() {
+	struct TEST t;
+	struct TEST *pt = &t;
+
+	pt->c = 0;
+	add_one(&t.c);
+
+	printf("t.c : %d \n",t.c);
+
+	add_one(&pt->c);
+	printf("t.c : %d \n" , t.c);
+	return 0;
+}
+
+int add_one(int *a) {
+	*a += 1;
+	return 0;
+}*/
+
+/*
+struct TEST {
+	int i;
+	char c;
+};
+
+int main() {
+	struct TEST st , st2;
+	st.i = 1;
+	st.c = 'c';
+
+	st2 = st;
+
+	printf("st2.i : %d \n", st2.i);
+	printf("st2.c : %c \n" , st2.c);
+
+	return 0;
+
+}*/
+
+/* 구조체 대입
+char copy_str(char *dest , char *src);
+struct TEST {
+	int i;
+	char str[20];
+};
+
+int main() {
+
+    struct TEST a, b;
+
+    b.i = 3;
+    copy_str(b.str, "hello , world");
+    a=b;
+
+    printf("a.str : %s \n", a.str);
+    printf("a.i : %d \n", a.i);
+
+    return 0;
+
+}
+
+char copy_str(char *dest , char *src) {
+
+    while (*src) {
+
+	*dest = *src;
+	src++;
+	dest++;
+
+    }
+
+    *dest = '\0';
+    return 1;
+}*/
+
+struct TEST {
+	int age;
+	int gender;
+};
+
+int set_human(struct TEST *a, int age , int gender);
+int main() {
+	struct TEST human;
+	set_human(&human , 10 , 1);
+	printf("AGE : %d // Gender : %d", human.age , human.gender);
+	return 0;
+
+}
+
+int set_human(struct TEST *a , int age , int gender) {
+	a->age = age;
+	a->gender = gender;
+	return 0;
 }
