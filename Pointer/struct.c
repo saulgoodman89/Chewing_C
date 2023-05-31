2d10faac86278431cefa814277571c508d05bc97
EungGyo <eunggyo@hutek.net>
2023-05-31 19:42:12 +0900

    void 추가

--- Pointer/struct.c
+++ Pointer/struct.c
@@ -178,6 +178,7 @@ char copy_str(char *dest , char *src) {
     return 1;
 }*/
 
+/*
 struct TEST {
 	int age;
 	int gender;
@@ -196,4 +197,45 @@ int set_human(struct TEST *a , int age , int gender) {
 	a->age = age;
 	a->gender = gender;
 	return 0;
+}*/
+
+struct TEST{
+
+    int age;
+    int gender;
+    char name[20];
+};
+
+int set_human(struct TEST *a , int age , int gender , const char *name);
+char copy_str(char *dest, const char *src);
+
+int main() {
+
+    struct TEST human;
+    set_human(&human , 10 , 1 , "lee");
+    printf("AGE : %d  // Gender : %d // Name : %s \n",human.age , human.gender , human.name);
+    return 0;
+
+}
+
+
+int set_human(struct TEST *a , int age , int gender , const char *name) {
+
+    a->age = age;
+    a->gender = gender;
+    copy_str(a->name , name);
+    return 0;
+}
+
+char copy_str(char *dest , const char *src) {
+
+    while (*src) {
+
+	*dest = *src;
+	src++;
+	dest++;
+    }
+
+    *dest = '\0';
+    return 1;
 }
---
+++ Pointer/void.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    // void a;의 경우 메모리 사이즈 체크가 안되서 에러 발생   
+    void* a;
+    //void *a의 경우 포인터기 때문에 메모리 상에 8Byte 만큼 지정. 어떠한 형태의 포인터의 값도 담을 수 잇다. 
+    return 0;
+
+}
+
