35b2e685419d8c067a74eb55a6a6501b58be38b1
EungGyo <eungkyo89@gmail.com>
2024-01-08 23:12:45 +0900

    23.01.08 배열 복습(1)

--- Again/third.c
+++ Again/third.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+/*
 int main() {
 	int arr[5]; //크기가 5인 arr배열 초기화
 	int i, ave = 0;	// i , ave 변수 초기화.
@@ -16,4 +17,34 @@ int main() {
 	printf("전체 학생의 평균은 :%d \n", ave / 5);
 	return 0;
 
+}*/
+
+int main() {
+	int guess = 5;
+	int prime[1000]; // 크기 1000짜리 배열 초기화 
+
+	int index = 1;
+	int i;
+	int ok;
+	prime[0] = 2;
+	prime[1] = 3;
+
+	for(;;) {
+		ok=0;
+		for(int i=0; i<=index; i++){
+			if(guess % prime[i] != 0) {
+				ok++;
+			}else {
+				break;
+			}
+		}
+		if( ok == (index + 1)) {
+			index++;
+			prime[index] = guess;
+			printf("소수 : %d \n", prime[index]);
+			if(index == 999) break;
+		}
+		guess += 2;
+	}
+	return 0;
 }
