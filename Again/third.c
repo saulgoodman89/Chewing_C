0e044978b50446eb97e4241a0f4167824d0fc18d
EungGyo <eunggyo@hutek.net>
2024-01-15 18:43:43 +0900

    23.01.15 복습 #1

--- Again/third.c
+++ Again/third.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 
+int str_length(char *str) {
+	int i = 0;
+	while (str[i]) {
+		i++;
+	}
+	return i;
+}
+
 int main() {
-	
+
+    char str[] = {"What is your name?"};
+
+    printf("이 문자열의 길이 : %d \n", str_length(str));
+    //char sentence_1[4] = {'P','s','i','\0'};
+    //char sentence_2[4] = {'P','s','i',0};
+    //char sentence_3[4] = {'P','s','i',(char)NULL};
+    //char sentence_4[4] = {"Psi"};
+//
+    //printf("sentence_1 : %s \n",sentence_1);
+    //printf("sentence_2 : %s \n",sentence_2);
+    //printf("sentence_3 : %s \n",sentence_3);
+    //printf("sentence_4 : %s \n",sentence_4);
+	////char null_1 = '\0';
+	//char null_2 = 0;
+	//char null_3 = (char)NULL;
+//
+	//char not_null = '0';
+//
+	//printf("NULL의 정수(아스키 값 : %d , %d , %d \n",null_1,null_2,null_3);
+	//printf("'0'의 정수(아스키) 값 : %d \n" , not_null);
+//
+	return 0;
 }
 /*
 int main() {
